85188ab11a5bed6ff3e889e6d41da115d51eca86
Arikui <arikui.ruby@gmail.com>
2014-05-24 07:46:08 +0900

    testing compiler

--- main.d
+++ main.d
@@ -1,6 +1,21 @@
 import std.stdio;
 
+import ando.readstream;
+import ando.parser;
+import ando.compiler;
+import ando.node;
+
+
 void main(){
-    "Hello!".writeln();
+    ando.compiler.testing();
+
+    /*
+    auto src = "
+-1;
+";
+    auto parser = new Parser(new Lexer(new StringReadStream(src), "(test)"));
+    parser.parse();
+    dump_tree(parser.tree);
+    */
 }
 
